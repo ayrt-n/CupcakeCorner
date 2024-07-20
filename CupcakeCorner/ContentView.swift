27b8a5baae7ebf8c43e9b028af6b6c27bbba85ad
ayrton <ayrton.parkinson1@gmail.com>
2024-07-20 14:28:22 -0400

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Ayrton Parkinson on 2024/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ayrton Parkinson on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
